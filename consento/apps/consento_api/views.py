588a7f410488cf7329f5ab21fe94fe820a53ae45
jacobis <remind.jacob@gmail.com>
2014-08-21 18:18:02 +0900

    Added get_restaurants function

--- consento/apps/consento_api/views.py
+++ consento/apps/consento_api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import requests
+import json
+from bs4 import BeautifulSoup as bs
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def get_restaurants(request):
+
+    # Get Parameter from URL.
+    vid = request.GET['vid']
+    query = request.GET['query']
+
+    # Get Cite, State from locate.
+    locate = request.GET['locate'].split(',')
+    cite = locate[0]
+    state = locate[1]
+
+    t = 'l'
+    wt = 'xml'
+
+    # Create URL.
+    url = 'http://9platters.com/s'
+    params = {'q': query, 't': t, 'wt': wt, 'ostate': state, 'ocity': cite}
+
+    # Get XML from 9platters.
+    response = requests.get(url, params=params)
+    response = response.content
+
+    # Parse XML.
+    response = bs(response)
+    objects = response.find_all('object')
+    objects = [obj.get('id') for obj in objects]
+
+    # Make Context.
+    json_dic = {'objects': objects}
+    context = json.dumps(json_dic)
+
+    return HttpResponse(context, content_type='application/json')
--- consento/urls.py
+++ consento/urls.py
@@ -12,3 +12,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('',
+    url(r'^restaurants/$', 'apps.consento_api.views.get_restaurants'),
+)
